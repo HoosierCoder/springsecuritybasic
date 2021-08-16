e24bf2f21216b0cf4a4e3a8e68d2e538840d8ab8
HoosierCoder <djohnstts@gmail.com>
2021-08-15 23:46:56 -0400

    initial commit

---
+++ src/main/java/com/eazybytes/springsecuritybasic/SprintsecuritybasicApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintsecuritybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintsecuritybasicApplication.class, args);
+	}
+
+}
